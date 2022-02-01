a3da03e14803bc9fc09464354e692ce153c3edb0
Guido Zalles <zallesguido1@gmail.com>
2022-02-01 07:54:17 -0400

    Adicionado tarea 2

---
+++ CLASE 2/pithonSyntax.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 07:21:34 2022
+
+@author: User
+"""
+
+
+# comentarios en linea
+
+"""
+comentarios en bloques o multilinea
+linea 1
+linea 2
+"""
+# crear una constante
+
+NAME = 'Guido'
+FULL_NAME = 'Guido Zalles Choque'
+
+print (NAME)
+print (FULL_NAME)
+
+# tipos de dato
+entero = 90
+print (f'el entero tiene valor de {entero}')
+decimal = 6.14
+print (f'el entero tiene valor de {decimal}')
+caracter = 'J'
+print (f'el entero tiene valor de {caracter}')
+cadena = 'primera cadena'
+print (f'el entero tiene valor de {cadena}')
+cadena_2 = 'segunda cadena'
+print (f'el entero tiene valor de {cadena_2}')
+
+booleano = False # valores True o False
+print (f'el valor del decimal es {booleano}')
+# lista
+list=[1, 2, 3, 4]
+print (f'el valor de la lista es : {list}')
+# diccionario
+diccionario = {'nombre':'Guido Zalles',"edad":26}
+nombre = diccionario["nombre"]
+edad = diccionario ["edad"]
+print (f'el nombre es : {nombre} y su edad {edad}')
+
+# entradas estandar
+
+telefono = input("Ingrese el numero telefonico")
+a = int(input ("Ingrese a :"))
+b = int(input ("Ingrese b :"))
+
+print(f'resultado de suma a + b = {a+b}')
+
+##
+
+
+
+
