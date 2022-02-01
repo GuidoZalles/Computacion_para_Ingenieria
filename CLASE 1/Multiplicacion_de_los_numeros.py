eddd4fadfabf7de1c57a71ffadfd368c1e552e11
Guido Zalles <zallesguido1@gmail.com>
2022-02-01 07:14:12 -0400

    Agregado Tarea multiplicar

---
+++ CLASE 1/Multiplicacion_de_los_numeros.py
@@ -0,0 +1,10 @@
+# COMPUTACION PARA INGENIERIA
+# CURSO DE VERANO
+# 01-02-2022
+# TAREA_2-MULTIPLICAR_LOS_3_PRIMEROS_NUMEROS
+
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+num3 = float(input("Ingrese el tercer numero: "))
+multiplicacion = num1*num2*num3
+print("La multiplicacion es: ", multiplicacion)
---
+++ CLASE 1/suma_de_los_3_primeros_numeros.py
@@ -0,0 +1,11 @@
+# COMPUTACION PARA INGENIERIA
+# CURSO DE VERANO
+# 01-02-2022
+
+i=1
+s=0
+while i<=3:
+    s = s+i
+    i=i+1
+
+print(" La suma es: ",s)
