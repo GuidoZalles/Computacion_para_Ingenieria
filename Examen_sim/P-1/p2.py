ddfca473abb066653bdf6d101154b3cce2dceb44
Guido Zalles <zallesguido1@gmail.com>
2022-02-07 08:17:16 -0400

    Agregado pregunta 2

---
+++ Examen_sim/P-1/p2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 08:07:50 2022
+
+@author: User
+"""
+
+# Pregunta 2
+
+list = [10, 20, 30, 10, 5, 1, 3, 5, 4]
+
+
+for num in list: #numeros pares
+    if num % 2 == 0:
+        print("los numeros pares son: ", num)
+        
+for num in list: # numeros impares
+    if num % 2 == 1:
+        print("los numeros impares son: ", num)
