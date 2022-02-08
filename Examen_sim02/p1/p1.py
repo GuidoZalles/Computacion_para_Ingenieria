dde8f8175248d11ec9ebf02a72d9fefce5c3859f
Guido Zalles <zallesguido1@gmail.com>
2022-02-08 09:47:03 -0400

    Add pregunta 1

---
+++ Examen_sim02/p1/p1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 09:01:47 2022
+
+@author: User
+"""
+
+# Pregunta 1
+# Pedir un parrafo al usuario y contar los espacios en el parrafo
+
+
+def contar_espacio(cadena):
+    espacio = 0
+    
+    for i in cadena:
+        if i == " ":
+            espacio = espacio + 1
+        
+    return espacio
+
+palabra = input("Ingrese un frase: ")
+print("la cantidad de espacios del parrafo es: ",contar_espacio(palabra) )
+        
+    
+    
