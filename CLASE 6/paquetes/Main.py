0f09a98aad1cd1c3f4a5252070013b0ab590b85a
Guido Zalles <zallesguido1@gmail.com>
2022-02-11 09:56:44 -0400

    Add paquetes

---
+++ CLASE 6/paquetes/Main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 09:25:18 2022
+
+@author: User
+"""
+
+import matematica.aritmetica
+
+print(matematica.aritmetica.sumar(2, 4))
+from matematica import aritmetica
+print(aritmetica.sumar(6, 6))
+
+from matematica.aritmetica import sumar
+print(sumar(2, 3))
+
+ # mportamos geometria
+ 
+ 
---
+++ CLASE 6/paquetes/matematica/geometria.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 09:21:54 2022
+
+@author: User
+"""
+
+def calcularArea():
+    print("calculando Area")
+    
+    def calcularDist(x,y):
+        print(f"la distancia es {x,y}")
+        
+
