4d34a20727ddb59d6cff473c1f34f6e932ec7f96
Guido Zalles <zallesguido1@gmail.com>
2022-02-11 09:10:49 -0400

    Add aritmetica and Main

---
+++ CLASE 6/modulos/Main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 09:01:06 2022
+
+@author: User
+"""
+
+# imprtr modulo
+
+import aritmetica
+print(aritmetica.sumar(1, 1))
+print(aritmetica.div(1, 1))
+
+print(aritmetica.restar(1, 1))
---
+++ CLASE 6/modulos/aritmetica.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 08:57:08 2022
+
+@author: User
+"""
+
+def sumar(a,b):
+    return a+b
+def restar(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    return a/b
