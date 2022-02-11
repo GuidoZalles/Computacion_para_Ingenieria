cf7b8f76ffd26b364deca32820547ef10e7330be
Guido Zalles <zallesguido1@gmail.com>
2022-02-11 09:19:08 -0400

    Corregido aritmetica and Main

--- CLASE 6/modulos/Main.py
+++ CLASE 6/modulos/Main.py
@@ -10,5 +10,20 @@ Created on Fri Feb 11 09:01:06 2022
 import aritmetica
 print(aritmetica.sumar(1, 1))
 print(aritmetica.div(1, 1))
-
 print(aritmetica.restar(1, 1))
+print('')
+# otra manera de importar
+
+from aritmetica import sumar, div
+
+print(sumar(1,1))
+print(div(1,1))
+print('')
+# importar todas la funcones de la libreria
+
+from aritmetica import *
+
+print(sumar(1,1))
+print(restar(2, 3))
+print(mult(2, 3))
+print(div(5, 2))
